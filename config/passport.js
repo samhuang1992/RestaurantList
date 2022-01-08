c35d42b2e7e39cab29d06b302f1ad67dddebdb4f
samhuang1992 <bob60117@gmail.com>
2022-01-08 23:34:39 +0800

    feat: add passport.js

---
+++ config/passport.js
@@ -0,0 +1,61 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, req.flash('warning_msg', '這個 Email 尚未註冊!'))
+        }
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('warning_msg', 'Email 或 密碼 錯誤!'))
+            }
+            return done(null, user)
+          })
+      })
+      .catch(err => done(err, false))
+  }))
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(error => done(error, false))
+      })
+  }))
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+}
