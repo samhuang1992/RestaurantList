087f996dfd703de45e3dd4bf98aae8123c80a733
samhuang1992 <bob60117@gmail.com>
2022-01-08 23:19:42 +0800

    feat: add register page

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const router = express.Router()
 const home = require('./modules/home')
 // 引用/routes/modules/restaurants.js中所設定的「首頁」路由
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
